09f265163755fcfa87b24cd1cf144b13cffdaefe
stormyy00 <jonathan.trujillo0432@gmail.com>
2025-09-04 23:46:38 -0700

    docker compose + email smtp

---
+++ src/utils/email.ts
@@ -0,0 +1,33 @@
+import * as nodemailer from "nodemailer";
+import SMTPTransport from "nodemailer/lib/smtp-transport";
+
+const transporter = nodemailer.createTransport({
+  host: (process.env.NEXT_PUBLIC_SMTP_HOST as string) ?? "",
+  port: (process.env.NEXT_PUBLIC_SMTP_PORT as unknown as number) ?? 25,
+  secure: false, 
+  auth: {
+    user: (process.env.NEXT_PUBLIC_SMTP_USER as string) ?? "",
+    pass: (process.env.NEXT_PUBLIC_SMTP_PASS as string) ?? "",
+  },
+});
+
+type emailProps = {
+  to: string;
+  subject: string;
+  text?: string;
+};
+
+export const sendEmail = async ({
+  to,
+  subject,
+  text,
+}: emailProps): Promise<SMTPTransport.SentMessageInfo> => {
+  const fromLine = process.env.NEXT_PUBLIC_SMTP_FROM ?? "no-reply";
+
+  return await transporter.sendMail({
+    from: fromLine,
+    to: to,
+    subject: subject,
+    text: text,
+  });
+};
