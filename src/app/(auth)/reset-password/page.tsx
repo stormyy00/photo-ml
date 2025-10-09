60fb20aa9784b1290c2202d5b29088e8b70806b6
stormyy00 <jonathan.trujillo0432@gmail.com>
2025-10-08 22:31:06 -0700

    fixed build error

--- src/app/(auth)/reset-password/page.tsx
+++ src/app/(auth)/reset-password/page.tsx
@@ -1,15 +1,4 @@
-"use client";
-import Reset from "@/components/auth/reset";
 import ResetPassword from "@/components/auth/reset-passowrd";
-import Error from "@/components/error";
-import { useSearchParams } from "next/navigation";
-import React from "react";
+export default ResetPassword;
 
-const page = () => {
-  const searchParams = useSearchParams();
-  const token = searchParams.get("token");
-
-  return token ? <Reset token={token} /> : <ResetPassword />;
-};
-
-export default page;
+export const dynamic = "force-dynamic";
